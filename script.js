b4addfba105c5d37365b663c01ee43193a4ea6df
Rbkcozzy <Muhammadakbarmaulana798@gmail.com>
2025-06-26 11:14:05 +0800

    Nest Loop 3.0

--- script.js
+++ script.js
@@ -1,11 +1,18 @@
 var s = ('');
 
-for (var i = 5; i >= 0; i--) {
+for (var i = 0; i <= 4; i++ ) {
+
     for (var j = 0; j <= i ; j++) {
         s += '*'
 }
  s += '\n'
 
+}
+for (var i = 5; i >= 0; i--) {
+    for (var j = 0; j <= i ; j++) {
+        s += '*'
+    }
+    s += '\n'
 }
 
 console.log(s);
