7263921e4f652fb1979e2ea473773e9c9105ae09
Rbkcozzy <Muhammadakbarmaulana798@gmail.com>
2025-06-26 14:29:47 +0800

    Loop Nest 4

--- script.js
+++ script.js
@@ -1,18 +1,37 @@
-var s = ('');
+var s = '\n';
 
-for (var i = 0; i <= 4; i++ ) {
+// Segitiga naik (refleksi Y)
+for (var i = 0; i <= 5; i++) {
 
-    for (var j = 0; j <= i ; j++) {
-        s += '*'
-}
- s += '\n'
-
-}
-for (var i = 5; i >= 0; i--) {
-    for (var j = 0; j <= i ; j++) {
-        s += '*'
+    
+    // Tambahkan spasi di depan
+    for (var k = 0; k < 5 - i; k++) {
+        s += ' ';
     }
-    s += '\n'
+
+
+    for (var j = 0; j <= i; j++) {
+       
+        s += '*';
+    }
+    
+     for (var j = 1; j <= i; j++) {
+       
+        s += '*';
+    }
+    s += '\n';
 }
+
+
 
 console.log(s);
+
+
+
+
+
+
+
+
+
+
