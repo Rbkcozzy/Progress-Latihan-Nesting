96c08ea80a054e26675fa11314630e2e4d717b70
Rbkcozzy <Muhammadakbarmaulana798@gmail.com>
2025-06-28 14:58:21 +0800

    Pascal ( dari git copilot )

--- script.js
+++ script.js
@@ -1,16 +1,28 @@
+var rows = 6;
+var pascal = [];
+
+// Membuat segitiga Pascal
+for (var i = 0; i < rows; i++) {
+    pascal[i] = [];
+    for (var j = 0; j <= i; j++) {
+        if (j === 0 || j === i) {
+            pascal[i][j] = 1;
+        } else {
+            pascal[i][j] = pascal[i - 1][j - 1] + pascal[i - 1][j];
+        }
+    }
+}
+
+// Menampilkan segitiga Pascal
 var s = '';
-
-for (var i = 0; i <= 6 ; i++) 
-
-{   if (i % 2 === 0) {
+for (var i = 0; i < rows; i++) {
+    // Menambah spasi agar segitiga rata tengah
+    for (var k = 0; k < rows - i - 1; k++) {
         s += ' ';
     }
-    
-
-    for (var k = 1; k <= 6; k++) {
-        s += ' #';
+    for (var j = 0; j <= i; j++) {
+        s += pascal[i][j] + ' ';
     }
-
     s += '\n';
 }
 
